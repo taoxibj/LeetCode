96f9d4e3ffc501da443eeb99b76271e2d9decb95
taoxi <173798637@qq.com>
2019-12-18 21:19:55 +0800

    提交

--- src/test/java/com/example/leecode/DemoApplicationTests.java
+++ src/test/java/com/example/leecode/DemoApplicationTests.java
@@ -7,7 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void testRemoveDuplicateItem() {
+	    int[] nums = {0,0,1,1,2,2,2,3};
+        int totals = RemoveDuplicateItem.removeDuplicates(nums);
+        System.out.println("totals:" + totals );
+        for (int i = 0; i < totals; i++) {
+            System.out.print(nums[i] + ",");
+        }
+    }
+
 
 }
