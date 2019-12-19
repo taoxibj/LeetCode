83703f30e08f1447b1615c1f1497bac14a631c78
taoxi <173798637@qq.com>
2019-12-19 20:50:13 +0800

    旋转数组

---
+++ src/main/java/com/example/leecode/RotateArray.java
@@ -0,0 +1,50 @@
+package com.example.leecode;
+
+public class RotateArray {
+    public static void rotateSolution1(int[] nums, int k) {
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
+        }
+    }
+
+    public static void rotateSolution2(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+
+    public static void rotateSolution3(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+}
--- src/test/java/com/example/leecode/DemoApplicationTests.java
+++ src/test/java/com/example/leecode/DemoApplicationTests.java
@@ -16,5 +16,31 @@ class DemoApplicationTests {
         }
     }
 
+    @Test
+    void testRotateArray1() {
+        int[] nums = {0,2,3,4,5,6,7,8};
+        RotateArray.rotateSolution1(nums, 2);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + ",");
+        }
+    }
+
+    @Test
+    void testRotateArray2() {
+        int[] nums = {0,2,3,4,5,6,7,8};
+        RotateArray.rotateSolution2(nums, 2);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + ",");
+        }
+    }
+
+    @Test
+    void testRotateArray3() {
+        int[] nums = {0,2,3,4,5,6,7,8};
+        RotateArray.rotateSolution3(nums, 2);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + ",");
+        }
+    }
 
 }
